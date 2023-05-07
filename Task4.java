08bda04e210c6268ff5c92b19230607236a04b16
LERA <111573064+Lera1811@users.noreply.github.com>
2023-05-07 22:41:06 +0300

    Add files via upload

---
+++ Task4.java
@@ -0,0 +1,26 @@
+package HomeWork2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/** Разработайте программу, которая выбросит Exception, когда пользователь вводит пустую строку.
+ * Пользователю должно показаться сообщение, что пустые строки вводить нельзя. */
+public class Task4 {
+    public static void main(String[] args) throws IOException {
+        Task4 task4 = new Task4();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder text;
+        System.out.print("Input your string: ");
+        text  = task4.inputText(reader);
+        System.out.println(ANSIConstants.ANSI_GREEN + "Input accepted: " + text.toString()
+                + ANSIConstants.ANSI_RESET);
+    }
+    public StringBuilder inputText(BufferedReader r) throws IOException {
+        StringBuilder s = new StringBuilder(r.readLine());
+        if (s.length() == 0) {
+            throw new IOException("Input can not be empty!");
+        }
+        return s;
+    }
+}
